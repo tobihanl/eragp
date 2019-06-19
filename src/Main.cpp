73f9c8891bf9aa9379f3bbfdb6b9cb179495f3c3
Tobias Wothge <tobias.wothge@tum.de>
2019-06-19 14:40:26 +0200

    https://trello.com/c/LcRcnKbw Implement main loop width adaptable fps, prioritizing the ticks() of the world not the actual rendering.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,58 +1,77 @@
-//
-// Created by jonas on 05.06.2019.
-//
-
+#include <iostream>
+#include <chrono>
 #include <SDL.h>
 #include "Renderer.h"
+#include "World.h"
+
+#define WINDOW_WIDTH 960
+#define WINDOW_HEIGHT 720
+#define MS_PER_TICK 100
 
 int main() {
-    Renderer::setup(640, 480);
+    Renderer::setup(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    // Render all needed textures
-    SDL_Texture *img = Renderer::renderImage("background.bmp");
-    SDL_Texture *hello = Renderer::renderFont("Hello World!", 20, {255, 255, 255, 0}, "font.ttf");
-    SDL_Texture *text = Renderer::renderFont("I am an evolution simulating program", 20, {255, 255, 255, 0},
-                                             "font.ttf");
+    // Instantiate a World object
+    auto *world = new World();
 
-    // Position all the textures
-    SDL_Rect dstImg = {250, 100, 0, 0};
-    SDL_Rect dstHello = {10, 10, 0, 0};
-    SDL_Rect dstText = {10, 30, 0, 0};
-    SDL_QueryTexture(img, nullptr, nullptr, &dstImg.w, &dstImg.h);
-    SDL_QueryTexture(hello, nullptr, nullptr, &dstHello.w, &dstHello.h);
-    SDL_QueryTexture(text, nullptr, nullptr, &dstText.w, &dstText.h);
+    bool render;
+    int lag = 0, currentTime, elapsedTime;
+    int previousTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
 
-    // Example Event Loop for letting SDL show up a window with some content
+    //=============================================================================
+    //                               BEGIN MAIN LOOP
+    //=============================================================================
     SDL_Event e;
     bool run = true;
-    while (run) {
+    while (true) {
+        render = false;
+
+        // Calculate lag between last and current turn
+        currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch()).count();
+        elapsedTime = (currentTime - previousTime);
+        previousTime = currentTime;
+        lag += elapsedTime;
+
+        // Process input
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT)
                 run = false;
         }
 
-        Renderer::clear();
+        if (!run) break;
 
-        Renderer::renderDot(100, 100, 5, {255, 0, 0, 0});
-        Renderer::renderDot(200, 100, 5, {0, 255, 0, 0});
-        Renderer::renderDot(100, 200, 5, {0, 0, 255, 0});
-        Renderer::renderDot(200, 200, 5, {255, 255, 255, 0});
+        // Calculate missing ticks
+        while (lag >= MS_PER_TICK) {
+            lag -= MS_PER_TICK;
 
-        Renderer::copy(img, &dstImg);
-        Renderer::copy(hello, &dstHello);
-        Renderer::copy(text, &dstText);
+            render = true;
+            world->tick();
+        }
 
-        Renderer::present();
+        // Render if needed
+        if (render) {
+            // Render everything
+            Renderer::clear();
+            world->render();
+            Renderer::present();
+        }
 
-        // 2 frames-per-second
-        SDL_Delay(1000 / 5);
+        // Calculate elapsed time of this loop turn
+        currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch()).count();
+        elapsedTime = (currentTime - previousTime);
+
+        // Wait if loop is too fast
+        if (elapsedTime <= MS_PER_TICK)
+            SDL_Delay(MS_PER_TICK - elapsedTime);
     }
+    //=============================================================================
+    //                                END MAIN LOOP
+    //=============================================================================
 
-    // Important: Clean-up all the textures!
-    Renderer::cleanup(img);
-    Renderer::cleanup(hello);
-    Renderer::cleanup(text);
-
+    // Destroy renderer (close window) and exit
     Renderer::destroy();
     return 0;
 }
