a23b12ae1f2fd83a43284ac70810ae5765143157
Tobias Wothge <tobias.wothge@tum.de>
2019-06-29 13:48:30 +0200

    https://trello.com/c/G8GihNlp Add scan of program arguments with GNU getopt(). Put main loop into its own function.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <SDL.h>
+#include <mpi.h>
+#include <unistd.h>
 #include "Renderer.h"
 #include "World.h"
 
@@ -8,11 +10,13 @@
 #define WINDOW_HEIGHT 720
 #define MS_PER_TICK 100
 
-int main() {
+/**
+ * Event Loop that is also rendering and updating the world.
+ *
+ * @param world The world, which should be updated and rendered
+ */
+void renderLoop(World &world) {
     Renderer::setup(WINDOW_WIDTH, WINDOW_HEIGHT);
-
-    // Instantiate a World object
-    auto *world = new World();
 
     bool render;
     int lag = 0, currentTime, elapsedTime;
@@ -47,14 +51,14 @@ int main() {
             lag -= MS_PER_TICK;
 
             render = true;
-            world->tick();
+            world.tick();
         }
 
         // Render if needed
         if (render) {
             // Render everything
             Renderer::clear();
-            world->render();
+            world.render();
             Renderer::present();
         }
 
@@ -73,5 +77,53 @@ int main() {
 
     // Destroy renderer (close window) and exit
     Renderer::destroy();
-    return 0;
+}
+
+/**
+ * =============================================================================
+ *                                 MAIN FUNCTION
+ * =============================================================================
+ */
+int main(int argc, char **argv) {
+    int height = 0;
+    int width = 0;
+    int c = 0;
+
+    // START MPI
+    MPI_Init(&argc, &argv);
+
+    // Scan program arguments
+    while ((c = getopt(argc, argv, "h:w:")) != -1) {
+        switch (c) {
+            case 'h':
+                height = strtol(optarg, nullptr, 10);
+                break;
+
+            case 'w':
+                width = strtol(optarg, nullptr, 10);
+                break;
+
+            case '?':
+                if (optopt == 'h' || optopt == 'w')
+                    std::cerr << "Option -h and -w require an integer!" << std::endl;
+                else
+                    std::cerr << "Unknown option character -" << (char) optopt << std::endl;
+
+                return EXIT_FAILURE;
+
+            default:
+                std::cout << "default" << std::endl;
+                break;
+        }
+    }
+
+    // Instantiate a World object
+    World world;
+
+    // Render-Event Loop
+    renderLoop(world);
+
+    // END MPI
+    MPI_Finalize();
+    return EXIT_SUCCESS;
 }
