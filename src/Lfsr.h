589ec83033c785067172103249bee4351b01838c
Tobias Wothge <tobias.wothge@tum.de>
2019-12-26 14:47:25 +0100

    Improve LFSR to use output bits for randoms
    https://trello.com/c/a53UJOKh

--- src/Lfsr.h
+++ src/Lfsr.h
@@ -7,14 +7,15 @@ class LFSR {
 private:
     uint64_t lfsr_register;
 
-    void step() {
+    uint8_t step() {
         /*
          * Taps at [64, 63, 61, 60] taken from
          * https://web.archive.org/web/20161007061934/http://courses.cse.tamu.edu/csce680/walker/lfsr_table.pdf
          * to get a MAXIMUM SIZE cycle (2^64 - 1, zero excluded)
          */
-        uint64_t bit = lfsr_register ^(lfsr_register >> 1u) ^(lfsr_register >> 3u) ^(lfsr_register >> 4u);
-        lfsr_register = (lfsr_register >> 1u) | (bit << 63u);
+        uint64_t bit = (lfsr_register ^ (lfsr_register >> 1u) ^ (lfsr_register >> 3u) ^ (lfsr_register >> 4u)) << 63u;
+        lfsr_register = (lfsr_register >> 1u) | bit;
+        return (bit != 0) ? 0x1u : 0x0u;
     }
 
 public:
@@ -23,11 +24,11 @@ public:
     explicit LFSR(uint64_t lfsr) : lfsr_register(lfsr) {}
 
     explicit LFSR(uint32_t seed) {
-        lfsr_register = static_cast<uint64_t>(seed) << 32u | static_cast<uint64_t>(seed);
+        lfsr_register = static_cast<uint64_t>(seed);
     }
 
     void seed(uint32_t s) {
-        lfsr_register = static_cast<uint64_t>(s) << 32u | static_cast<uint64_t>(s);
+        lfsr_register = static_cast<uint64_t>(s);
     }
 
     uint64_t getLfsrRegister() {
@@ -35,8 +36,10 @@ public:
     }
 
     uint32_t getNextInt() {
-        step();
-        return static_cast<uint32_t>(lfsr_register >> 32u);
+        uint32_t result = 0x0u;
+        for (uint8_t i = 0; i < 32; i++)
+            result |= static_cast<uint32_t>(step()) << i;
+        return result;
     }
 
     uint32_t getNextIntBetween(uint32_t from, uint32_t to) {
