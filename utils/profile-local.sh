47b1e98abf6ae9c24b13a97331e243a776bad154
Jonas Jürß <jonas.juerss@tum.de>
2020-01-08 17:35:00 +0100

    limited perf output

--- utils/profile-local.sh
+++ utils/profile-local.sh
@@ -5,5 +5,5 @@ rank=$(printenv PMI_RANK)
 datafile="perf/perf.data.$rank"
 outfile="perf/perf.$rank.svg"
 
-perf record -o $datafile --call-graph dwarf ./build/Evolution $@
+perf record -o $datafile --call-graph dwarf,2048 ./build/Evolution $@
 perf script -i $datafile | ./utils/stackcollapse-perf.pl | ./utils/flamegraph.pl > $outfile
